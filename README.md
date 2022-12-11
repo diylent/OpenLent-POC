2f1d611342b5c27e4f67195d5bcd29ea2c6e523d
abe <abe@valle.us>
2022-12-10 21:39:16 -0600

    init commit - putting together the nutrition label function

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# OpenLent-POC
-This is a simple proof concept for the OpenLent project. This POC will demonstrate the feasibility of an recepie sharing platform.
+# OpenLent Proof of Concept
+
+#####What is OpenLent?
+OpenLent is the opensource complete meal sharing platform for users.
+
+##### What can we expect form this POC?
+Not much this POC will be a very simple volitile example of what OpenLent can be. Create your recepies by entering your ingriedients 
+
+#####Goals for this Proof of Concept.
+---
+* Recepie creating function
+* Nutrion Label generation based on the USDA nutrion label tool
+* Recepie storage function 
---
+++ src/index.js
@@ -0,0 +1,223 @@
+nutrientProfile = {
+    macroNutrients: {
+        calories: {
+            ammount: 0,
+            max: 0,
+            unit: "kcal"
+        },
+        carbohydrates: {
+            ammount: 0,
+            max: 0,
+            unit: "g"
+        },
+        protien: {
+            ammount: 0,
+            max: 0,
+            unit: "g"
+        },
+        // Total Fat must be calculate from the other Fat
+        fats: {
+            totalFat: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            saturatedFat: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            monounsaturatedFat: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            polyunsaturatedFat: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            omega3FattyAcids: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            omega6FattyAcids: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },    
+        },
+        fibers: {
+            totalFiber: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            solubleFiber: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+            insolubleFiber: {
+                ammount: 0,
+                max: 0,
+                unit: "g"
+            },
+        },
+        cholestoral: {
+            ammount: 0,
+            max: 0,
+            unit: "mg"
+        },
+    },
+    vitamins: {
+        A: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "IU",
+        },
+        B6: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        B12: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "ug",
+        },
+        C: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        D: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "IU",
+        },
+        E: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "IU",
+        },
+        K: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "ug",
+        },
+        thlamin: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        riboflavin: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        niacin: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        folate: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        pantothenicAcid: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+        biotin: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "ug",
+        },
+        choline: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg",
+        },
+    },
+    minerals: {
+        calcium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        chloride: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        chromium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg"
+        },
+        copper: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg"
+        },
+        iodine: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "ug"
+        },
+        iron: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg"
+        },
+        magnesium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "mg"
+        },
+        manganese: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        molybdenum: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        phosphorus: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        potassium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        selenium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        sodium: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        sulfur: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+        zinc: {
+            ammount: 0,
+            max: 0,
+            measurementUnit: "g"
+        },
+    }
+}
